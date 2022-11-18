ed55c6efaa583ecb41f8ca3ae06ad46272494bd9
Jack Edmondson <99421203+Edmndsn@users.noreply.github.com>
2022-11-18 18:08:58 +0000

    .

--- components/Work/PhotographyData.js
+++ components/Work/PhotographyData.js
@@ -9,4 +9,179 @@ export const PhotographyData = [
 		src: "/Photography/photog2.jpg",
 		position: "Horizontal",
 	},
+	{
+		id: 3,
+		src: "/Photography/photog3.jpg",
+		position: "Portrait",
+	},
+	{
+		id: 4,
+		src: "/Photography/photog4.jpg",
+		position: "Portrait",
+	},
+	{
+		id: 5,
+		src: "/Photography/photog5.jpg",
+		position: "Portrait",
+	},
+	{
+		id: 6,
+		src: "/Photography/photog6.jpg",
+		position: "Portrait",
+	},
+	{
+		id: 7,
+		src: "/Photography/photog7.jpg",
+		position: "Horizontal",
+	},
+	{
+		id: 8,
+		src: "/Photography/photog8.jpg",
+		position: "Portrait",
+	},
+	{
+		id: 9,
+		src: "/Photography/photog9.jpg",
+		position: "Horizontal",
+	},
+	{
+		id: 10,
+		src: "/Photography/photog10.jpg",
+		position: "Horizontal",
+	},
+	{
+		id: 11,
+		src: "/Photography/photog11.jpg",
+		position: "Portrait",
+	},
+	{
+		id: 12,
+		src: "/Photography/photog12.jpg",
+		position: "Portrait",
+	},
+	{
+		id: 13,
+		src: "/Photography/photog13.jpg",
+		position: "Portrait",
+	},
+	{
+		id: 14,
+		src: "/Photography/photog14.jpg",
+		position: "Portrait",
+	},
+	{
+		id: 15,
+		src: "/Photography/photog15.jpg",
+		position: "Portrait",
+	},
+	{
+		id: 16,
+		src: "/Photography/photog16.jpg",
+		position: "Portrait",
+	},
+	{
+		id: 17,
+		src: "/Photography/photog17.jpg",
+		position: "Portrait",
+	},
+	{
+		id: 18,
+		src: "/Photography/photog18.jpg",
+		position: "Portrait",
+	},
+	{
+		id: 19,
+		src: "/Photography/photog19.jpg",
+		position: "Portrait",
+	},
+	{
+		id: 20,
+		src: "/Photography/photog20.jpg",
+		position: "Portrait",
+	},
+	{
+		id: 21,
+		src: "/Photography/photog21.jpg",
+		position: "Portrait",
+	},
+	{
+		id: 22,
+		src: "/Photography/photog22.jpg",
+		position: "Portrait",
+	},
+	{
+		id: 23,
+		src: "/Photography/photog23.jpg",
+		position: "Portrait",
+	},
+	{
+		id: 24,
+		src: "/Photography/photog24.jpg",
+		position: "Portrait",
+	},
+	{
+		id: 25,
+		src: "/Photography/photog25.jpg",
+		position: "Portrait",
+	},
+	{
+		id: 26,
+		src: "/../public/photography/photog26.jpg",
+		position: "Portrait",
+	},
+	{
+		id: 27,
+		src: "/Photography/photog27.jpg",
+		position: "Portrait",
+	},
+	{
+		id: 28,
+		src: "/Photography/photog28.jpg",
+		position: "Portrait",
+	},
+	{
+		id: 29,
+		src: "/Photography/photog29.jpg",
+		position: "Portrait",
+	},
+	{
+		id: 30,
+		src: "/Photography/photog30.jpg",
+		position: "Portrait",
+	},
+	{
+		id: 31,
+		src: "/Photography/photog31.jpg",
+		position: "Portrait",
+	},
+	{
+		id: 32,
+		src: "/Photography/photog32.jpg",
+		position: "Portrait",
+	},
+	{
+		id: 33,
+		src: "/Photography/photog33.jpg",
+		position: "Portrait",
+	},
+	{
+		id: 34,
+		src: "/Photography/photog34.jpg",
+		position: "Portrait",
+	},
+	{
+		id: 35,
+		src: "/Photography/photog35.jpg",
+		position: "Portrait",
+	},
+	{
+		id: 36,
+		src: "/Photography/photog36.jpg",
+		position: "Portrait",
+	},
+	{
+		id: 37,
+		src: "/Photography/photog37.jpg",
+		position: "Portrait",
+	},
 ];
