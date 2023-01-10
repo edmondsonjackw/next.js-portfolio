9e89928a7ff44d630f64cb2d54c4969ecd716935
Edmndsn <edmondson.jackw@gmail.com>
2023-01-10 13:01:27 +0000

    .

--- pages/About.js
+++ pages/About.js
@@ -44,8 +44,8 @@ const About = () => {
         <Skills name="CSS3" value="CSS3" />
         <Skills name="JavaScript ES6" value="JavaScript" />
         <Skills name="React" value="React" />
-        <Skills name="Next-JS" value="Next.JS" />
-        <Skills name="Node.JS" value="TailwindCSS" />
+        <Skills name="Next.js" value="Next.JS" />
+        <Skills name="Node.js" value="Node.js" />
         <Skills name="REST APIs" value="TailwindCSS" />
         <Skills name="TailwindCSS" value="TailwindCSS" />
         <Skills name="Bootstrap" value="TailwindCSS" />
